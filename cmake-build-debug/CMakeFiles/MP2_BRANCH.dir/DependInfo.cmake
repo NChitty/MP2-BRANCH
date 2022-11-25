7c02ae2de98fd5d62d05b4015816661ef802b149
Nicholas Chitty <nicholas.chitty@gmail.com>
2022-11-24 20:59:42 -0500

    clion garbage

---
+++ cmake-build-debug/CMakeFiles/MP2_BRANCH.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nicholas/Documents/MP2-BRANCH/src/main.cpp" "CMakeFiles/MP2_BRANCH.dir/src/main.cpp.o" "gcc" "CMakeFiles/MP2_BRANCH.dir/src/main.cpp.o.d"
+  "/Users/nicholas/Documents/MP2-BRANCH/src/predictors/BimodalPredictor.cpp" "CMakeFiles/MP2_BRANCH.dir/src/predictors/BimodalPredictor.cpp.o" "gcc" "CMakeFiles/MP2_BRANCH.dir/src/predictors/BimodalPredictor.cpp.o.d"
+  "/Users/nicholas/Documents/MP2-BRANCH/src/predictors/GsharePredictor.cpp" "CMakeFiles/MP2_BRANCH.dir/src/predictors/GsharePredictor.cpp.o" "gcc" "CMakeFiles/MP2_BRANCH.dir/src/predictors/GsharePredictor.cpp.o.d"
+  "/Users/nicholas/Documents/MP2-BRANCH/src/predictors/HybridPredictor.cpp" "CMakeFiles/MP2_BRANCH.dir/src/predictors/HybridPredictor.cpp.o" "gcc" "CMakeFiles/MP2_BRANCH.dir/src/predictors/HybridPredictor.cpp.o.d"
+  "/Users/nicholas/Documents/MP2-BRANCH/src/predictors/SmithPredictor.cpp" "CMakeFiles/MP2_BRANCH.dir/src/predictors/SmithPredictor.cpp.o" "gcc" "CMakeFiles/MP2_BRANCH.dir/src/predictors/SmithPredictor.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MP2_BRANCH.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MP2_BRANCH.dir/src/main.cpp.o"
+  "CMakeFiles/MP2_BRANCH.dir/src/main.cpp.o.d"
+  "CMakeFiles/MP2_BRANCH.dir/src/predictors/BimodalPredictor.cpp.o"
+  "CMakeFiles/MP2_BRANCH.dir/src/predictors/BimodalPredictor.cpp.o.d"
+  "CMakeFiles/MP2_BRANCH.dir/src/predictors/GsharePredictor.cpp.o"
+  "CMakeFiles/MP2_BRANCH.dir/src/predictors/GsharePredictor.cpp.o.d"
+  "CMakeFiles/MP2_BRANCH.dir/src/predictors/HybridPredictor.cpp.o"
+  "CMakeFiles/MP2_BRANCH.dir/src/predictors/HybridPredictor.cpp.o.d"
+  "CMakeFiles/MP2_BRANCH.dir/src/predictors/SmithPredictor.cpp.o"
+  "CMakeFiles/MP2_BRANCH.dir/src/predictors/SmithPredictor.cpp.o.d"
+  "MP2_BRANCH"
+  "MP2_BRANCH.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MP2_BRANCH.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
